a5c72418291787f345d4c882bae159931b380e1b
Zeus <tchaipa97@gmail.com>
2024-09-08 21:32:17 +0200

    code update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { NavBar } from "./components/NavBar";
-// import Starfield from 'react-starfield';
 import { Banner } from "./components/Banner";
 import { Languages } from "./components/Languages";
 import AnimatedCursor from "react-animated-cursor";
 import React, { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { StarField } from "starfield-react";
 // import RecipeReviewCard from "./components/RecipeReviewCard";
 
 function App() { 
   return (
     <div className="App">
-      {/* <Starfield
+      <StarField
         starCount={1000}
         starColor={[255, 255, 255]}
         speedFactor={0.25}
         backgroundColor="black"
-      /> */}
+      />
       <NavBar />
       <Banner />
       <AnimatedCursor />
